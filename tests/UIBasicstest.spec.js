bb31a14f47767f4771ff4f20aef44dc012635abe
Dini Na <dna975@UoA.auckland.ac.nz>
2026-06-17 20:59:29 +1200

    browser context and Page

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -1,5 +1,13 @@
 const {test} = require('@playwright/test');
 
-test('First Playwright test',async ()=>{
-    
+
+test('Browser Context Playwright test',async ({browser})=>{
+    //chrome - open new borwser without any cookie / cache
+    const context = await browser.newContext();
+    const page = await context.newPage();
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
+});
+
+test('Page Playwright test', async({page})=>{
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
 });
