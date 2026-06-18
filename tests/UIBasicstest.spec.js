5715221966e0fc1deeacbbd07b65a39daa14b149
Dini Na <dna975@UoA.auckland.ac.nz>
2026-06-18 21:15:34 +1200

    using locators and extact multiple webelements in page

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -5,17 +5,26 @@ test('Browser Context Playwright test',async ({browser})=>{
     //chrome - open new borwser without any cookie / cache
     const context = await browser.newContext();
     const page = await context.newPage();
+    const userName = page.locator("#username");
+    const signIn = page.locator("#signInBtn");
+
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     console.log( await page.title());
 
     //css, xpath
-    await page.locator("#username").fill("dini");
-    await page.locator("[type='password']").fill("learning");
-    await page.locator("#signInBtn").click();
+    await userName.fill("dini na");
+    await page.locator("[type='password']").fill("Learning@830$3mK2");
+    await signIn.click();
 
     //it takes some time until warning message to be displayed
     console.log(await page.locator("[style*='block']").textContent());
     await expect(page.locator("[style*='block']")).toContainText('Incorrect');
+
+    //type-fill
+    await userName.fill(""); //wipe out
+    await userName.fill("rahulshettyacademy");
+    await signIn.click();
+    console.log(await page.locator(".card-body a").first().textContent());
 });
 
 test('Page Playwright test', async({page})=>{
