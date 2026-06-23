c238d8e0ba30d024ce2fd7ac911aa37da3290fa7
Dini Na <dna975@UoA.auckland.ac.nz>
2026-06-23 19:11:13 +1200

    dynamically find the order from order history page

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -124,4 +124,16 @@ test.only('Client App login', async({page})=>{
    await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ");
    const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
    console.log(orderId);
+   await page.locator("button[routerlink*='myorders']").click();
+   await page.locator("tbody").waitFor();
+   const rows = page.locator("tbody tr");
+   for (let i = 0; i < await rows.count(); ++i) {
+      const rowOrderId = await rows.nth(i).locator("th").textContent();
+      if (orderId.includes(rowOrderId)) {
+         await rows.nth(i).locator("button").first().click();
+         break;
+      }
+   }
+   const orderIdDetails = await page.locator(".col-text").textContent();
+   expect(orderId.includes(orderIdDetails)).toBeTruthy();
 });
